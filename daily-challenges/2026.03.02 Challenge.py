4b5e561bca053adb3834c75edb5643dc9f699411
Akunor <samuel.stoddard@gmail.com>
2026-03-02 15:18:35 +0000

    Daily Challenge 2nd Mar 2026

---
+++ daily-challenges/2026.03.02 Challenge.py
@@ -0,0 +1,15 @@
+# start of main.py
+
+def sum_letters(s):
+    s = s.upper()
+    score = 0
+    values = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z':26}
+
+    for char in s:
+        if char.isalpha():
+            score += values[char]
+
+    return score
+
+# end of main.py
+
